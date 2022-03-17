59a45cf1cdeed93e2f9624156cefb0e37c988ac6
Andrew Trice <amtrice@us.ibm.com>
2022-03-17 12:45:34 -0400

    added namespace to validation script
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>

--- .github/scripts/validate-deploy.sh
+++ .github/scripts/validate-deploy.sh
@@ -11,6 +11,6 @@ echo "checking for portworx services"
 
 oc rollout status deployment/portworx-operator -n kube-system
 STORAGECLUSTER=$(kubectl get storagecluster -n kube-system -o=jsonpath='{.items[].metadata.name}' )
-oc rollout status storagecluster/$STORAGECLUSTER
+oc rollout status storagecluster/$STORAGECLUSTER -n kube-system
 
 exit 0
