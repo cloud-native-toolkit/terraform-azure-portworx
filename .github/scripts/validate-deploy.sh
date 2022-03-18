ebc99dafb979c9f1938aa9444690f489251e21cc
Andrew Trice <amtrice@us.ibm.com>
2022-03-18 09:59:15 -0400

    updated validation script
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>

--- .github/scripts/validate-deploy.sh
+++ .github/scripts/validate-deploy.sh
@@ -3,17 +3,18 @@
 set -e
 
 export KUBECONFIG=$(cat .kubeconfig)
-echo "sleeping for 2 mins to prevent synchronization errors"
-sleep 2m
+echo "sleeping to prevent synchronization errors"
+sleep 3m
 
 echo "checking for portworx services"
 
 
 oc rollout status deployment/portworx-operator -n kube-system
-STORAGECLUSTER=$(kubectl get storagecluster -n kube-system -o=jsonpath='{.items[].metadata.name}' )
 
-#sleep 10m
+#STORAGECLUSTER=$(kubectl get storagecluster -n kube-system -o=jsonpath='{.items[].metadata.name}' )
+#oc rollout status storagecluster.core.libopenstorage.org/$STORAGECLUSTER -n kube-system
 
-oc rollout status storagecluster.core.libopenstorage.org/$STORAGECLUSTER -n kube-system
+PX_POD=$(kubectl get pods -l name=portworx -n kube-system -o jsonpath='{.items[0].metadata.name}')
+kubectl exec $PX_POD -n kube-system -- /opt/pwx/bin/pxctl status
 
 exit 0
